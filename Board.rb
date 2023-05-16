9b86659ae8fbaae8447ba21c89c382f0eabee507
yong kim <yhkim4573@gmail.com>
2023-05-16 14:22:59 -0400

    working on memory game

--- Board.rb
+++ Board.rb
@@ -1,22 +1,35 @@
-require_relative Card
-require_relative Game 
+class Board
+    attr_accessor :grid
+
+    def initialize
+
+        @grid = Array.new(5) {Array.new(5,  " ")}
+    end 
 
 
-def initalize
-
-    @grid = Array.new(5) {Array.new(5,  " ")}
-end 
-
-
-def populate
-    card_arr = ("a".."h").to_a * 2 
-    @grid.each do |row|
-        row.each do |col|
-            letter = card_arr.sample
-            @grid[row][col] = letter
-            card_arr.delete_at(card_arr.index(letter))
+    def populate
+        card_arr = ("a".."h").to_a * 2 
+        (1...grid.length).each do |row|
+            (1...grid.length).each do |col|
+                letter = card_arr.sample
+                @grid[row][col] = letter
+                if letter == nil || card_arr.index(letter) == nil
+                    break
+                else 
+                    card_arr.delete_at(card_arr.index(letter))
+                end
+            end 
+        
         end 
     end 
-end 
 
+
+
+    def self.render 
+        @grid.each do |row|
+            p row
+        end
+    end 
+
+end 
 
